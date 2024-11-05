eebb88e73a01bc9375a48a6f5d9e4668c662abb9
NerOunt <kravlad568@gmail.com>
2024-11-05 22:11:38 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,27 @@
+function click1(event) {
+    event.preventDefault();
+
+    const goodSelect = document.getElementById('t');
+    const goodPrice = parseInt(goodSelect.value);
+
+    if (goodSelect.value === '') {
+        alert('Вы не выбрали товар');
+        return;
+    }
+
+    const goodAmount = parseInt(document.getElementById('amount').value);
+
+    if (isNaN(goodAmount) || goodAmount <= 0) {
+        alert('Введите  количество товара');
+        return;
+    }
+
+    const totalCost = goodPrice * goodAmount;
+
+    const res = document.getElementById('result');
+    res.textContent = 'Стоимость равна ' + totalCost;
+}
+
+// Убедитесь, что форма и обработчик событий подключены
+const form = document.getElementById('priceCalculator');
+form.addEventListener('submit', click1);
