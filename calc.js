f00942825400df945b722b8e696b9a07a34dc591
NerOunt <kravlad568@gmail.com>
2024-11-13 00:50:49 +0300

    calc.js

--- calc.js
+++ calc.js
@@ -18,9 +18,9 @@ function click1(event) {
 
     const totalCost = goodPrice * goodAmount;
 
-    const res = document.getElementById('result');
-    res.textContent = 'Стоимость равна ' + totalCost+ 'рублей';
-}
+    const res = document.getElementById('result'); 
+            res.textContent = 'Стоимость равна ' + totalCost + ' рублей'; 
+        }
 window.addEventListener('DOMContentLoaded', function (event) {
   console.log("DOM fully loaded and parsed");
   let b = document.getElementById("my-button");
